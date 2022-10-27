78b7446abceb563ac722688f5e4b0d194a9c578c
Jean-Baptiste Guerin <jb.l.guerin@gmail.com>
2022-10-27 15:16:34 +0200

    feat: add block not img file ext (NewBill.js)

--- bill-app/Billed-app-FR-Front/src/containers/NewBill.js
+++ bill-app/Billed-app-FR-Front/src/containers/NewBill.js
@@ -20,10 +20,12 @@ export default class NewBill {
     const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
     const filePath = e.target.value.split(/\\/g)
     const fileName = filePath[filePath.length-1]
+    if(fileName.split(".")[1] === 'jpg' || fileName.split(".")[1] === 'png' || fileName.split(".")[1] === 'jpeg') {
     const formData = new FormData()
     const email = JSON.parse(localStorage.getItem("user")).email
     formData.append('file', file)
     formData.append('email', email)
+    console.log(fileName)
     this.store
       .bills()
       .create({
@@ -38,6 +40,10 @@ export default class NewBill {
         this.fileUrl = fileUrl
         this.fileName = fileName
       }).catch(error => console.error(error))
+    }
+    else {
+      this.document.querySelector(`input[data-testid="file"]`).value = "";
+    }
     }   
   handleSubmit = e => {
     e.preventDefault()
