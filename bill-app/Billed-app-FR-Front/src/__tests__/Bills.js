1be324b4c0b894fce4b0851fa32abe6c5d057f87
Jean-Baptiste Guerin <jb.l.guerin@gmail.com>
2022-11-04 17:16:35 +0100

    patch: add test bills.js files  78% statement

--- bill-app/Billed-app-FR-Front/src/__tests__/Bills.js
+++ bill-app/Billed-app-FR-Front/src/__tests__/Bills.js
@@ -5,12 +5,22 @@
 import {fireEvent, screen, waitFor} from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
-import { ROUTES_PATH} from "../constants/routes.js";
+import {ROUTES, ROUTES_PATH} from "../constants/routes.js";
 import {localStorageMock} from "../__mocks__/localStorage.js";
 import Bills, {handleClickNewBill, handleClickIconEye} from "../containers/Bills.js";
 
 import router from "../app/Router.js";
 import Actions from "../views/Actions.js";
+/*
+window.$ = jest.fn().mockImplementation(() => {
+  return {
+     click: jest.fn(),
+     width: jest.fn(),
+     find: jest.fn()
+     
+   }
+});
+*/
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -38,28 +48,26 @@ describe("Given I am connected as an employee", () => {
       expect(dates).toEqual(datesSorted)
     })
     
-    test("Then add event click for NewBill button", () => {
-      const handleClickNewBills = jest.fn(handleClickNewBill);
-      document.body.innerHTML = BillsUI({ data: bills });
-      const btn = screen.getByTestId("btn-new-bill"); 
-      btn.addEventListener("click", handleClickNewBills);
-      fireEvent.click(btn);
-      expect(btn).toBeDefined();
-      expect(handleClickNewBills).toHaveBeenCalledTimes(1)
-      
-    })
-
     test("Then go to newbill display", async () => {
       
       Object.defineProperty(window, 'localStorage', { value: localStorageMock })
       window.localStorage.setItem('user', JSON.stringify({
         type: 'Employee'
       }))
-      const root = document.createElement("div")
-      root.setAttribute("id", "root")
-      document.body.append(root)
-      router()
-      window.onNavigate(ROUTES_PATH.NewBill)
+      document.body.innerHTML = BillsUI({ data: bills })
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+      const store = null
+      const bils = new Bills({
+        document, onNavigate, store, bills, localStorage: window.localStorage
+      })
+      const btn = screen.getByTestId("btn-new-bill");
+      const handleClickNewBills = jest.fn((e) => bils.handleClickNewBill(e, bills[0]))
+      btn.addEventListener("click", handleClickNewBills)
+      fireEvent.click(btn)
+      expect(btn).toBeDefined();
+      expect(handleClickNewBills).toHaveBeenCalledTimes(1)
       await waitFor(() => screen.getByTestId('form-new-bill'))
       const formNewBill = screen.getByTestId('form-new-bill')
       expect(formNewBill).toBeTruthy()
@@ -84,19 +92,24 @@ describe("Given I am connected as an employee", () => {
       window.localStorage.setItem('user', JSON.stringify({
         type: 'Employee'
       }))
-      document.body.innerHTML = BillsUI({ data: bills })
-    
-      const handleClickIconEyes = jest.fn(handleClickIconEye)
-      const eyes = screen.getAllByTestId("icon-eye")
-      eyes.forEach(element => {
-        element.addEventListener('click', handleClickIconEyes);
-      });
-      fireEvent.click(eyes[0])
+      
+      document.body.innerHTML = BillsUI({data: bills})
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+      const store = null
+      const bils = new Bills({
+        document, onNavigate, store, localStorage: window.localStorage
+      })
+      $.fn.modal = jest.fn();
+      const handleClickIconEyes = jest.fn((e) => bils.handleClickIconEye)
+      
+      const eyes = screen.getAllByTestId("icon-eye")[0]
+      eyes.addEventListener("click", handleClickIconEyes)
+      fireEvent.click(eyes)
       expect(handleClickIconEyes).toHaveBeenCalledTimes(1)
+      expect($.fn.modal).toHaveBeenCalledTimes(1)
 
-      await waitFor(() => screen.getByTestId('modale-file'))
-      const modals = screen.getByTestId("modale-file")
-      expect(modals).toBeTruthy()
     })
 
 
